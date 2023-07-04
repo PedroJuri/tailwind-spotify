d076b1636187b8218027cbfb9c5e285a3d866cd6
PedroJuri <p3dr1n018@outlook.com>
2023-07-04 15:44:37 -0300

    realizando primeiras estilizações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home() {
   return (
-    <main></main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64 bg-zinc-950 p-6">
+          Sidebar
+        </aside>
+        <main className="flex-1 p-6">
+          Main
+        </main>
+      </div>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        Footer
+      </footer>
+    </div>
   )
 }

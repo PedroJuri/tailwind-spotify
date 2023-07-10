2fc06dbde1b749ec70328e7c7ad74ef841ae4776
PedroJuri <p3dr1n018@outlook.com>
2023-07-10 14:39:11 -0300

    feat: atualizando sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
+import { HomeIcon, Search, Library } from 'lucide-react'
+
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64 bg-zinc-950 p-6">
-          Sidebar
+        <aside className="w-72 bg-zinc-950 p-6">
+
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 bg-red-500 rounded-full"/>
+            <div className="w-3 h-3 bg-yellow-500 rounded-full"/>
+            <div className="w-3 h-3 bg-green-500 rounded-full"/>
+          </div>
+
+          <nav className='space-y-5 mt-10'>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <HomeIcon/>
+              Home
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Search/>
+              Search
+            </a>
+            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+              <Library/>
+              Your Library
+            </a>
+          </nav>
+
+          <nav className='mt-6 pt-10 border-t border-zinc-800 flex flex-col gap-3'>
+            <a className='text-sm text-zinc-400 hover:text-zinc-100' href="">Mix Melancólico</a>
+            <a className='text-sm text-zinc-400 hover:text-zinc-100' href="">Relaxing Songs</a>
+            <a className='text-sm text-zinc-400 hover:text-zinc-100' href="">De Cria</a>
+            <a className='text-sm text-zinc-400 hover:text-zinc-100' href="">Musicas que Resgatei do Relacionamento Antigo</a>
+          </nav>
+
         </aside>
         <main className="flex-1 p-6">
           Main

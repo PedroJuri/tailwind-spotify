f1e6ec8207288e2f78ab4c107437576290d0c002
PedroJuri <p3dr1n018@outlook.com>
2023-07-10 17:11:49 -0300

    feat: add made for 'user'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,6 @@ export default function Home() {
           </div>
           
           <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
-
           <div className='grid grid-cols-3 gap-4 mt-4'>
             <a href='#' className='bg-white/5 group rounded overflow-hidden flex items-center gap-4 hover:bg-white/10 transition-colors'>
               <Image src='/album.jpg' width={104} height={104} alt="Capa do álbum 'Quest for Fire' de Skrillex"/>
@@ -91,6 +90,35 @@ export default function Home() {
                 <Play fill='black'/>
               </button>
             </a>
+          </div>
+
+          <h2 className='font-semibold text-2xl mt-10'>Made for Pedro Santos</h2>
+          <div className='grid grid-cols-5 gap-4 mt-4'>
+            <a href='#' className="bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src='/album.jpg' className='w-full' width={120} height={120} alt="Capa do álbum 'Quest for Fire' de Skrillex"/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Zedd, Nora en Pure, Avicii and more</span>
+            </a> 
+            <a href='#' className="bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src='/album.jpg' className='w-full' width={120} height={120} alt="Capa do álbum 'Quest for Fire' de Skrillex"/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Zedd, Nora en Pure, Avicii and more</span>
+            </a> 
+            <a href='#' className="bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src='/album.jpg' className='w-full' width={120} height={120} alt="Capa do álbum 'Quest for Fire' de Skrillex"/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Zedd, Nora en Pure, Avicii and more</span>
+            </a> 
+            <a href='#' className="bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src='/album.jpg' className='w-full' width={120} height={120} alt="Capa do álbum 'Quest for Fire' de Skrillex"/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Zedd, Nora en Pure, Avicii and more</span>
+            </a> 
+            <a href='#' className="bg-white/5 p-2 rounded-md flex flex-col gap-2 hover:bg-white/10 transition-colors">
+              <Image src='/album.jpg' className='w-full' width={120} height={120} alt="Capa do álbum 'Quest for Fire' de Skrillex"/>
+              <strong className='font-semibold'>Daily Mix 1</strong>
+              <span className='text-sm text-zinc-400'>Zedd, Nora en Pure, Avicii and more</span>
+            </a> 
           </div>
         </main>
       </div>
